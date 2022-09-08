6903c75277c20a049b7e3e08e8b0fbc67019f114
Mario Coronado <chinocoronado2@gmail.com>
2022-09-07 21:18:40 -0300

    crea tu landing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
-
+import ItemListContainer from './container/ItemListContainer';
 
 
 function App() {
+  const message =("Los mejores productos para jugar al baloncesto⛹️‍♂️")
   return (
+    <>
     <NavBar/>
+    <ItemListContainer greeting={message}/>
+  </>
   );
 }
 
