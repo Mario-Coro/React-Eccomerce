5965f4a467cb724e86acb04991ca685d8c5829cc
Mario Coronado <chinocoronado2@gmail.com>
2022-10-13 19:59:25 -0300

    Item Collection II

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,32 +2,58 @@ import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { Context } from "../Context/CartContext";
 import Item from "../Item/Item";
-import './cart.css'
+import "./cart.css";
+import { db } from "../../firebase/firebase";
+import { collection, addDoc, serverTimestamp } from "firebase/firestore";
 
 const Cart = () => {
   const { cart, deleteItem, reset, total } = useContext(Context);
-  return(
+
+  const client = {
+    nombre: "Mario Coronado",
+    Telefono: 1144556677,
+    email: "chinocoronado181@gmail.com",
+  };
+
+  const checkOut = () => {
+    const salesCollection = collection(db, "sales");
+    addDoc(salesCollection, {
+      client,
+      items: cart,
+      date: serverTimestamp(),
+      total,
+    }).then((sale) => console.log(sale.id));
+  };
+
+  return (
     <>
-    {
-      cart.length === 0?( <h1>no hay productos. Ve al <Link to='/'>Home</Link> y añade productos</h1> ): 
-      <> {cart.map((item) => (
-        <div className="item" key={item.product.id}>
-          <img src={item.product.img} alt="" />
-          <h3>{item.product.name}</h3>
-          <h4>Cantidad: {item.quantity}</h4>
-          <button onClick={()=>{deleteItem(item.product.id)}}>
-            Eliminar del Carrito
-          </button>
-        </div>
-      ))}
-      <button onClick={reset}>Vaciar Carrito</button>
-      <h3>Total:${total}</h3>
+      {cart.length === 0 ? (
+        <h1>
+          no hay productos. Ve al <Link to="/">Home</Link> y añade productos
+        </h1>
+      ) : (
+        <>
+          {" "}
+          {cart.map((item) => (
+            <div className="item" key={item.product.id}>
+              <img src={item.product.img} alt="" />
+              <h3>{item.product.name}</h3>
+              <h4>Cantidad: {item.quantity}</h4>
+              <button
+                onClick={() => {
+                  deleteItem(item.product.id);
+                }}
+              >
+                Eliminar del Carrito
+              </button>
+            </div>
+          ))}
+          <button onClick={reset}>Vaciar Carrito</button>
+          <button onClick={checkOut}>Checkout</button>
+        </>
+      )}
     </>
-    }
-    
-    
-    
-    </>)
-}
+  );
+};
 
 export default Cart;
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -3,34 +3,33 @@ import ItemList from "../ItemList/ItemList";
 import React, { useEffect, useState } from "react";
 import { Spinner } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
-import { db } from '../../firebase/firebase';
+import { db } from "../../firebase/firebase";
 import { getDocs, collection, query, where } from "firebase/firestore";
 
 const ItemListContainer = () => {
   let { categoryId } = useParams();
   const [productList, setProductList] = useState([]);
   const [loading, setLoading] = useState(true);
-  const productsCollection = collection( db, 'products')
+  const productsCollection = collection(db, "products");
 
-  const getData =(prodList)=>{
-    getDocs(prodList)
-    .then((data)=>{
-      const list = data.docs.map((product)=>{
-        return{
-          ...product.data(), id: product.id
-        }
-      })
-      setProductList(list)
-      setLoading(false)
-    })
-  }
-
-  
+  const getData = (prodList) => {
+    getDocs(prodList).then((data) => {
+      const list = data.docs.map((product) => {
+        return {
+          ...product.data(),
+          id: product.id,
+        };
+      });
+      setProductList(list);
+      setLoading(false);
+    });
+  };
 
   useEffect(() => {
-     categoryId === undefined ? getData(productsCollection) : getData(query(productsCollection, where('category', '==', categoryId)))
-  },[categoryId]);
-
+    categoryId === undefined
+      ? getData(productsCollection)
+      : getData(query(productsCollection, where("category", "==", categoryId)));
+  }, [categoryId]);
 
   return (
     <main className="products-container">
